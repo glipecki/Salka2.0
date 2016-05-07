7f80adcb99a469aff09b94179d7162d5088b99e7
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2016-05-07 21:14:57 +0200

    organizing imports

--- salka-rest/src/main/java/net/lipecki/salka/spec/SwaggerConfiguration.java
+++ salka-rest/src/main/java/net/lipecki/salka/spec/SwaggerConfiguration.java
@@ -4,18 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.BasicAuth;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spi.service.contexts.SecurityContextBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
